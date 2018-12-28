9b396ce0ff2b12c7cb5497c5d6cea4ea6c794c17
Thomas Donnelly <donnelly.thomas21@gmail.com>
2018-12-28 11:20:02 -0500

    iPhone layout

---
+++ dev-profile/dev-profile/Views/RadiusImg.swift
@@ -0,0 +1,18 @@
+//
+//  RadiusImg.swift
+//  dev-profile
+//
+//  Created by Thomas Donnelly on 12/27/18.
+//  Copyright © 2018 Thomas Donnelly. All rights reserved.
+//
+
+import UIKit
+
+class RadiusImg: UIImage {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
